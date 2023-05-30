9e4fb07d8dc6d415cf4034b6c5ca1baf81ccefdf
hillary <kibet_hillary@yahoo.com>
2023-05-30 11:16:06 +0300

    nain configs

---
+++ .history/configs/api_credentials_20230530103638.py
@@ -0,0 +1,13 @@
+import streaml
+
+it
+
+TMDB_API_KEY = streamlit.secrets["TMDB_API_KEY"]
+YOUTUBE_API_KEY = streamlit.secrets["YOUTUBE_API_KEY"]
+OMDB_API_KEY = streamlit.secrets["OMDB_API_KEY"]
+
+"""
+TMDB_API_KEY = os.environ["TMDB_API_KEY"]
+YOUTUBE_API_KEY = os.environ["YOUTUBE_API_KEY"]
+OMDB_API_KEY = os.environ["OMDB_API_KEY"]
+"""
